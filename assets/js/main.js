52c0761b9634600742c6915b154d75585b48eac2
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-11 15:39:00 +0200

    end dice ex

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@ console.log("hello");
  //controlla che sia nella lista di chi può accedere, array con più email [array]
  //stampa un messaggio appropriato sull’esito del controllo. 
 
+  // commento esercizio uno MOMENTANEO
+/*
+
 const mailUser = prompt("Benvenuto, inserisci email")
 //console.log(mailUser);
 const usersMail = ['pinco.pallo@gmail.com', 'pallo.pinco@gmail.com', 'mario.rossi@gmail.com']
@@ -24,14 +27,26 @@ if(mailUser === usersMail[0]){
     console.log("Accessi Negato");
     advice.innerHTML = "Accesso Negato =("
 }
-
-
-
-
-
-
-
+ */
 
 //  Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const winner = document.querySelector("h2")
+
+const userNumber = Math.round(Math.random() * 6 + 1)
+console.log(userNumber, "Numero utente");
+const computerNumber = Math.round(Math.random() * 6 + 1)
+console.log(computerNumber, "Numero Computer");
+
+if(userNumber > computerNumber){
+    console.log("Utente, hai vinto!");
+    winner.innerHTML = "Utente, hai vinto! =)"
+} else{
+    console.log("Ha vinto il computer!")
+    winner.innerHTML = "Ha vinto il computer! =("
+}
+
+
+
