b71e47dd8f97fa5b89b0dd286d9591245566b18f
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-11 13:59:37 +0200

    initial commit, add html, css and js

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+    //Mail
+ //Chiedi all’utente la sua email, form
+ //controlla che sia nella lista di chi può accedere, array con più email
+ //stampa un messaggio appropriato sull’esito del controllo. 
+
+ 
+
+
+
+
+
+//  Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
